174520adc05eccdbed106a050589c64a77066c68
ArchiRocksTech <archirockstech@gmail.com>
2021-07-02 09:08:50 -0400

    updates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BTTspeedUtilities
 Various utilities to improve BTT income with uTorrent and Bittorrent torrent clients.
+
+# Torrent Pruning
+This PowerShell script will automatically prune torrents which exceed a certain age. Works will with automated methods of adding torrents so that you constantly have fresh content. Requires you enable the WebGUI. Configure the settings in the script prior to use.
+uTorrent-Prune-Old-Torrents.ps1
+
+# Ban non-BTT-Speed clients
+A PowerShell script that helps to ensure you maximize your income potential by only allowing clients which support BTT Speed. It offers to methods, the IPFilter.dat of the torrent client and the Windows Firewall.
---
+++ uTorrent-Prune-Old-Torrents.ps1
@@ -0,0 +1,107 @@
+# uTorrent Web GUI API interaction - PowerShell - Torrent Pruning
+#
+# To run, edit the settings below to match your client and desired pruning schedule.
+# Save and Exit, then right-click this file, and click Run with PowerShell.
+#
+# To stop the script, press CTRL+C.
+
+# WebUI Settings, as configured in your client
+# WebUI Username
+$user = 'admin'
+# WebUI Password
+$pass = 'abc123'
+# WebUI Port
+$port = '50000'
+
+# Prune old torrents? If $false, it will only list torrents and age. ($true or $false. Default: $false)
+$pruneTorrents = $false
+
+# Prune torrents older than X hours old. (Number. Default: 36)
+$pruneOlderThan = 36
+
+# Prune every X minutes. (Number. Default: 60)
+$reRunEveryXminutes = 60
+
+# Remove Torrent and Data, or just Torrent? ($true or $false. Default: $false)
+$removeData = $false
+
+# CHANGE NOTHING BELOW =================================================================
+# ======================================================================================
+$host.UI.RawUI.WindowTitle = "uTorrent-Prune-Old-Torrents"
+$baseURL = "http://127.0.0.1:$port/gui"
+$pair = "$($user):$($pass)"
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$Headers = @{
+    Authorization = $basicAuthValue
+    "Content-Type" = "text/plain"
+    "charset" = "utf-8"
+}
+Try {
+    $tokenResults = Invoke-WebRequest -Uri $($baseURL + '/token.html') -Headers $Headers -Method Get -WebSession $session
+    $regExOptions = [System.Text.RegularExpressions.RegexOptions]::Multiline
+    $regExFilter = '^Set-Cookie:\sGUID=([a-zA-Z0-9]+);'
+    $regExMatch = [regex]::Match($tokenResults.RawContent,$regExFilter,$regExOptions)
+    $cookieValue = $regExMatch.Groups[1].Value
+    $token = $tokenResults.ParsedHtml.body.innerText
+    $cookie = New-Object System.Net.Cookie 
+    $cookie.Name = "GUID"
+    $cookie.Value = $cookieValue
+    $cookie.Domain = "127.0.0.1"
+    $cookie.Path = '/'
+    $session.Cookies.Add($cookie);
+} Catch {
+    Write-Host "Failed to query token and cookie information from the Client." -ForegroundColor Red
+    Write-Host "Please ensure the configuration matches in the Client and this script and that the Client is currently running." -ForegroundColor Yellow
+    Pause
+    break
+}
+
+$tzOffset = ([TimeZoneInfo]::Local).BaseUtcOffset
+If ((get-date).IsDaylightSavingTime()){$dlsOffset = (Get-CimInstance win32_timezone).DaylightBias} Else {$dlsOffset = 0}
+
+$stopIt = $false
+
+Do {
+    Clear-Host
+    $list = Invoke-RestMethod -Uri $($baseURL + '/?list=1' + "&token=" + $token) -Headers $Headers -Method Get -WebSession $session
+    $info = $list.torrents | ConvertTo-Json | ConvertFrom-Json
+    $torrentHashes = @()
+
+    ForEach ($torrent in $info) {
+        $hash = $torrent.value[0]
+        $status = $torrent.value[21]
+        $name = $torrent.value[2]
+        #$addedOn = $torrent.value[23] # You can use the added on value, but its preferrable to use the completed on value
+        $completedOn = $torrent.value[24]
+        #$AddedOnFromEpoch = ((Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($addedOn)) + $tzOffset).AddMinutes(-$dlsOffset)
+        $CompletedOnFromEpoch =  ((Get-Date 01.01.1970)+([System.TimeSpan]::fromseconds($completedOn)) + $tzOffset).AddMinutes(-$dlsOffset)
+        $age = [math]::Round( (New-TimeSpan -Start $CompletedOnFromEpoch -End (Get-Date)).TotalHours,2)
+        Write-Host "[Age: $age hours] $name" -ForegroundColor Cyan
+        If ($age -ge $pruneOlderThan -and $status -eq "Seeding 100.0 %") {
+            Write-Host "Pruning due to age: $name" -ForegroundColor Yellow
+            $torrentHashes += $hash
+        }
+    }
+
+    $hashList = $null; $i = 0; $ii = 0  
+    ForEach ($hash in $torrentHashes) {
+        $hashList += "&hash=$hash"
+        $i ++ | Out-Null; $ii ++ | Out-Null        
+        If ($i -eq 35 -or $ii -eq $torrentHashes.Count) {
+            If ($pruneTorrents -eq $true) {
+                If ($removeData -eq $true) {
+                    Invoke-WebRequest -Uri $($baseURL + "/?action=removedata$hashList" + "&token=" + $token) -Headers $Headers -Method Get -WebSession $session | Out-Null
+                } Else {
+                    Invoke-WebRequest -Uri $($baseURL + "/?action=remove$hashList" + "&token=" + $token) -Headers $Headers -Method Get -WebSession $session | Out-Null
+                }
+            }
+            $hashList = $null; $i = 0
+        }
+    }
+
+    Write-Host "Sleeping..." -ForegroundColor Cyan
+    Start-Sleep -Seconds ($reRunEveryXminutes * 60)
+
+} Until ($stopIt -eq $true)
